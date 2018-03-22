585056f563cdb3a96e357a90a90223b13520c6e4
Anton Zobnin <zobninatn@gmail.com>
2018-03-22 17:28:39 +0300

    Redundant try/except removed

--- web/api/views.py
+++ web/api/views.py
@@ -15,24 +15,19 @@ class HashViewSet(viewsets.ViewSet):
     API endpoint that allow to start document hashing or to get result from it.
     """
     def retrieve(self, request, pk=None):
-        try:
-            res = AsyncResult(pk)
-            if res.state == 'SUCCESS':
-                content = {'state': res.state, 'result': res.get()}
-                return Response(content, status=status.HTTP_200_OK)
-            elif res.state == 'FAILURE':
-                try:
-                    res.get()
-                except Exception as e:
-                    content = {'state': res.state, 'cause': str(e)}
-                return Response(content, status=status.HTTP_400_BAD_REQUEST)
-            else:
-                content = {'state': res.state}
-                return Response(content, status=status.HTTP_409_CONFLICT)
-        except KeyError:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        except (ValueError, TypeError):
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        res = AsyncResult(pk)
+        if res.state == 'SUCCESS':
+            content = {'state': res.state, 'result': res.get()}
+            return Response(content, status=status.HTTP_200_OK)
+        elif res.state == 'FAILURE':
+            try:
+                res.get()
+            except Exception as e:
+                content = {'state': res.state, 'cause': str(e)}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            content = {'state': res.state}
+            return Response(content, status=status.HTTP_409_CONFLICT)
 
     def create(self, request):
         if 'url' in request.POST:
